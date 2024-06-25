4aafaa520b39be7a6dc8cdd0fae60b37c5a5b888
Parth Chawande <parthrc00@gmail.com>
2024-06-25 14:17:56 +0530

    Added login endpoint

--- server/src/controllers/authControllers.ts
+++ server/src/controllers/authControllers.ts
@@ -3,8 +3,45 @@ import UserModel from "../models/userModel";
 import bcrypt from "bcryptjs";
 import { createAvatarLink, generateJWT } from "../utils/helpers";
 
-const loginUser = (req: Request, res: Response) => {
+const loginUser = async (req: Request, res: Response) => {
   try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ status: "error", msg: "Please enter all required details" });
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ status: "error", msg: "Email does not exist" });
+    }
+
+    // check password
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res
+        .status(400)
+        .json({ status: "error", msg: "Incorrect password" });
+    }
+
+    // generate JWT and set cookie
+    const jwt = generateJWT(user._id as string);
+    res.cookie("chatapp-jwt", jwt, {
+      maxAge: 15 * 24 * 60 * 60 * 1000, // milliseconds
+      httpOnly: true, // prevent XSS attacks
+      sameSite: "strict", // prevent CSRF attacks
+      secure: process.env.NODE_ENV !== "development",
+    });
+
+    return res.status(200).json({
+      status: "success",
+      msg: "User registered successfully",
+    });
   } catch (error) {
     return res.status(500).json({ status: "error", msg: error.message });
   }
--- server/src/utils/helpers.ts
+++ server/src/utils/helpers.ts
@@ -4,11 +4,9 @@ import jwt from "jsonwebtoken";
  * API LINK: avatar-placeholder.iran.liara.run
  * Returns a link for Avatar Image With Initials From Names
  * @param firstName first anem of the user
- * @param lastName last name of the user
- *
+ * @param lastName last name of the user *
  * @returns api link for avatar image
  */
-
 const createAvatarLink = (firstName: string, lastName?: string): string => {
   const URL = lastName
     ? `https://avatar.iran.liara.run/username?username=${firstName}+${lastName}`
@@ -19,17 +17,14 @@ const createAvatarLink = (firstName: string, lastName?: string): string => {
 
 /**
  * Returns a JWT
- *cls
-
- * @param userId userId od the user
- *
+ * @param userId userId od the user *
  * @returns JWT
  */
 const generateJWT = (userId: string): string => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
-//   console.log("JWT:", token);
+  //   console.log("JWT:", token);
   return token;
 };
 
