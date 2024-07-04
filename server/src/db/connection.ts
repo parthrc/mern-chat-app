953dd247a62dd6d7b2e702f871dc27396789946e
Parth Chawande <parthrc00@gmail.com>
2024-07-04 15:12:30 +0530

    Added docker compose

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI_DOCKER);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB: ", error.message);
   }
 };
 
-export default connectToMongoDB
+export default connectToMongoDB;
